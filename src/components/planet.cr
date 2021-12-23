6b39ce1550a9deb43a699e6862595e22856873c3
Arthur POULET <arthur.poulet@thetrainline.com>
2021-12-24 00:21:49 +0100

    improve galaxy generator with moons

--- src/components/planet.cr
+++ src/components/planet.cr
@@ -1,55 +1,10 @@
 class Planet
-  BODIES_STATISTICS = (0..10)
+  # keep it low and > 0 for dev
+  BODIES_STATISTICS = (1..3)
 
   # TODO don't copy symbols like this
   TYPES_STATISTICS = %i(asteroid_belt planet planet planet)
 
-  def self.generate(context : GameContext, star : Entitas::IEntity)
-    # star_name = star.get_component Named.index_val
-    # star_position = star.get_component Position.index_val
-    star_name = star.named.name
-    star_position = star.position
-
-    bodies_amount = BODIES_STATISTICS.sample
-    asteroid_belt_id = 0
-    planet_id = 0
-    bodies = bodies_amount.times.map do |index|
-      body = context.create_entity
-      star_position.as(Position).copy_to body
-      body.add_stellar_position body_index: index + 1, moon_index: 0
-      # TODO add moon generator
-
-      body_type = TYPES_STATISTICS.sample
-      body.add_celestial_body type: body_type
-
-      populated_proba = 0.0
-      if body_type == :asteroid_belt
-        asteroid_belt_id += 1
-        body.add_named name: "#{star_name} Belt #{asteroid_belt_id}"
-      else
-        planet_id += 1
-        populated_proba = 0.3
-        body.add_named name: "#{star_name} #{planet_id}"
-      end
-
-      pop_amount = 0.0
-      if rand > populated_proba
-        pop_amount = ((10_000.0)..(10_000_000_000.0)).sample
-      end
-      body.add_population amount: pop_amount
-
-      body.add_resources(
-        storages: {
-          :food => { amount: 0.0, max: 1000.0 },
-          :mineral => { amount: 0.0, max: 10000.0 },
-          :alloy => { amount: 0.0, max: 1000.0 },
-        },
-        productions: {
-          { input: nil, output: :food } => { rate: 1.0, max_speed: 20.0 },
-          { input: nil, output: :mineral } => { rate: 1.0, max_speed: 10.0 },
-          { input: :mineral, output: :alloy } => { rate: 0.2, max_speed: 2.0 },
-        },
-      )
-    end.to_a
-  end
+  # keep it low for dev
+  MOONS_STATISTICS = (0..2)
 end
--- src/systems/galaxy_generator.cr
+++ src/systems/galaxy_generator.cr
@@ -7,21 +7,94 @@ class GalaxyInitializerSystem
 
   def init
     stars = SYSTEMS_AMOUNT.times.map do |i|
-      star = @context.create_entity
-      star.add_celestial_body type: :star
-      Position.generate star
-      Named.generate_star star
+      generate_star
     end.to_a
-    group = @context.get_group Entitas::Matcher.all_of Position
-    STDERR.puts "init #{group.entities.size} stars"
-    group.entities.each { |entity| puts "new Star [#{entity.named.to_s}] at [#{entity.position.to_s.to_s}]" }
+    stars = @context.get_group Entitas::Matcher.all_of(Named, Position, CelestialBody).none_of(StellarPosition)
+    stars.entities.each { |entity| puts "new Star [#{entity.named.to_s}] at [#{entity.position.to_s.to_s}]" }
 
-    stars.each do |star|
-      Planet.generate @context, star
+    bodies = @context.get_group Entitas::Matcher.all_of(Named, Position, CelestialBody, StellarPosition)
+    bodies.entities.each { |entity| puts "new Body [#{entity.named.to_s}] in [#{entity.stellar_position.body_index}, #{entity.stellar_position.moon_index}]" }
+  end
+
+  private def generate_star
+    ids_trash = {
+      :asteroid_belt => 0,
+      :planet        => 0,
+      :moon          => 0,
+    }
+    
+    star = @context.create_entity
+    star.add_celestial_body type: :star
+    Position.generate star
+    Named.generate_star star
+    
+    bodies_amount = Planet::BODIES_STATISTICS.sample
+    bodies_amount.times.map do |index|
+      body_type = Planet::TYPES_STATISTICS.sample
+      ids_trash[body_type] += 1
+      body = generate_body(star: star, index: index, body_type: body_type, ids_trash: ids_trash)
+      if body_type == :asteroid_belt
+        body_to_asteroid_belt(star: star, body: body, index: index, body_index: ids_trash[body_type])
+      else
+        body_to_planet(star: star, body: body, index: index, body_index: ids_trash[body_type])
+      end
+    end.to_a
+  end
+
+  private def generate_body(star : Entitas::IEntity, index : Int32, body_type : Symbol, ids_trash : Hash(Symbol, Int32))
+    star_position = star.position
+
+    body = @context.create_entity
+    star_position.as(Position).copy_to body
+    body.add_stellar_position body_index: index, moon_index: ids_trash[:moon]
+
+    body.add_celestial_body type: body_type
+    body
+  end
+
+  private def body_to_planet(star, body, index, body_index, moon_index = nil)
+    star_name = star.named.name
+    moon_particule = moon_index ? ("a".."z").to_a[moon_index] : ""
+    body.add_named name: "#{star_name} #{body_index}#{moon_particule}"
+    if moon_index.nil?
+      moon_amount = Planet::MOONS_STATISTICS.sample
+      moon_amount.times.map do |moon_time_index|
+        ids_trash_with_moon = {
+          :asteroid_belt => 0,
+          :planet        => 0,
+          :moon          => moon_time_index + 1,
+        }
+        moon_body = generate_body(star: star, index: index, body_type: :planet, ids_trash: ids_trash_with_moon)
+        body_to_planet(star: star, body: moon_body, index: index, body_index: body_index, moon_index: moon_time_index + 1)
+        moon_body
+      end.to_a
     end
 
-    group = @context.get_group Entitas::Matcher.all_of Named, Position, StellarPosition
-    STDERR.puts "init #{group.entities.size} bodies"
-    group.entities.each { |entity| puts "new Body [#{entity.named.to_s}] in [#{entity.stellar_position.body_index}]" }
+    populate(body) if rand < 0.3
+    body
+  end
+
+  private def body_to_asteroid_belt(star, body, index, body_index)
+    star_name = star.named.name
+    body.add_named name: "#{star_name} Belt #{body_index}"
+    body
+  end
+
+  def populate(body)
+    pop_amount = ((10_000.0)..(10_000_000_000.0)).sample
+    body.add_population amount: pop_amount
+    body.add_resources(
+      storages: {
+        :food => { amount: 0.0, max: 1000.0 },
+        :mineral => { amount: 0.0, max: 10000.0 },
+        :alloy => { amount: 0.0, max: 1000.0 },
+      },
+      productions: {
+        { input: nil, output: :food } => { rate: 1.0, max_speed: 20.0 },
+        { input: nil, output: :mineral } => { rate: 1.0, max_speed: 10.0 },
+        { input: :mineral, output: :alloy } => { rate: 0.2, max_speed: 2.0 },
+      },
+    )
+    body
   end
 end
