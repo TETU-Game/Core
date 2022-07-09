7e9528129d11b6515b72198bee0cb58e9f4bc684
Arthur Poulet <arthur.poulet@sceptique.eu>
2022-07-09 20:23:49 +0200

    Add a manpower empty panel

--- src/systems/ui/planet.cr
+++ src/systems/ui/planet.cr
@@ -9,18 +9,25 @@ class TETU::UiPlanetSystem
     draw_planet(planet)
   end
 
-  private def draw_planet(planet : Entitas::Entity)
-    draw_planet_frame do
-      ImGui.text planet.named.name
+  # NOTE: this is a weird pattern, maybe stick to not parameterize the panels?
+  private def draw_planet(planet : GameEntity)
+    draw_planet_frame(planet, ->draw_resources(GameEntity), ->draw_manpower(GameEntity))
+  end
 
-      if planet.has_resources?
-        draw_storage planet
-        ImGui.text ""
-        draw_infras planet
-      else
-        ImGui.text "Barren..."
-      end
+  private def draw_resources(planet : GameEntity)
+    ImGui.text planet.named.name
+
+    if planet.has_resources?
+      draw_storage planet
+      ImGui.text ""
+      draw_infras planet
+    else
+      ImGui.text "Barren..."
     end
+  end
+
+  private def draw_manpower(planet : GameEntity)
+    ImGui.text "draw manpower"
   end
 
   private def draw_storage(planet)
@@ -138,11 +145,18 @@ class TETU::UiPlanetSystem
 
   LEFT_SIDEBAR_SIZE = TETU::UI_CONF["left_sidebar"].as_i64
 
-  private def draw_planet_frame(&block)
+  private def draw_planet_frame(planet, infra_ui, manpower_ui)
     if ImGui.begin(name: "left side", flags: ImGui::ImGuiWindowFlags.new(0))
       ImGui.set_window_pos("left side", ImGui::ImVec2.new(0, 0))
       ImGui.set_window_size("left side", ImGui::ImVec2.new(LEFT_SIDEBAR_SIZE, Window::GALAXY_HEIGHT))
-      yield
+      if ImGui.tree_node_ex("resources panel", ImGui::ImGuiTreeNodeFlags.new(ImGui::ImGuiTreeNodeFlags::DefaultOpen))
+        infra_ui.call(planet)
+        ImGui.tree_pop
+      end
+      if ImGui.tree_node_ex("manpower panel", ImGui::ImGuiTreeNodeFlags.new(ImGui::ImGuiTreeNodeFlags::DefaultOpen))
+        manpower_ui.call(planet)
+        ImGui.tree_pop
+      end
     end
     ImGui.end
   end
